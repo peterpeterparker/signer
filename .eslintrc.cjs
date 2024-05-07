2347ae5095d962fd370a4ad81c37fa5bddb494ed
David Dal Busco <david.dalbusco@outlook.com>
2024-05-07 15:16:26 +0200

    chore: lint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,5 +27,20 @@ module.exports = {
 				parser: '@typescript-eslint/parser'
 			}
 		}
-	]
+	],
+	rules: {
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': [
+			'warn',
+			{
+				argsIgnorePattern: '^_',
+				varsIgnorePattern: '^_',
+				caughtErrorsIgnorePattern: '^_'
+			}
+		],
+		'no-console': ['error', { allow: ['error', 'warn'] }]
+	},
+	globals: {
+		NodeJS: true
+	}
 };
