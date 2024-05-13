90c4ec93ce954880547b61046db1dbfabe0aa47e
David Dal Busco <david.dalbusco@outlook.com>
2024-05-13 15:57:02 +0200

    feat: response permissions

--- src/relying_party_frontend/src/lib/icrc-wallet.ts
+++ src/relying_party_frontend/src/lib/icrc-wallet.ts
@@ -7,7 +7,8 @@ import {
 	type IcrcWalletPermissionsRequestType
 } from '$core/types/icrc';
 import { popupTopRight } from '$core/utils/window.utils';
-import {JSON_RPC_VERSION_2} from "$core/types/rpc";
+import {JSON_RPC_VERSION_2, RpcRequest, RpcResponse} from "$core/types/rpc";
+import {nonNullish} from "@dfinity/utils";
 
 export class IcrcWallet {
 	private constructor(private walletOrigin: string | undefined) {}
@@ -20,26 +21,45 @@ export class IcrcWallet {
 				popupTopRight({ width: WALLET_POPUP_WIDTH, height: WALLET_POPUP_HEIGHT })
 			);
 
-			const onMessage = ({ data, origin }: MessageEvent<Partial<IcrcWalletNotificationType>>) => {
+			let walletOrigin: string | undefined = undefined;
 
+			const onMessage = ({ data, origin }: MessageEvent<Partial<IcrcWalletNotificationType>>) => {
 				console.log(data, origin);
 
-				// Parse throw an error if not expected msg.
-				const notification = IcrcWalletNotification.parse(data);
-				console.log(notification);
+				if (nonNullish(walletOrigin) && walletOrigin !== origin) {
+					// TODO
+					throw new Error("Invalid origin");
+				}
 
-				const msg: IcrcWalletPermissionsRequestType = {
-					jsonrpc: JSON_RPC_VERSION_2,
-					method: ICRC25_REQUEST_PERMISSIONS,
-					params: {
-						scopes: [{ method: ICRC27_GET_ACCOUNTS }]
-					}
-				};
+				walletOrigin = origin;
 
-				popup?.postMessage(msg, { targetOrigin: origin });
+				const {success: isRpcRequest} = RpcRequest.safeParse(data);
 
-				// TODO:
-				// popup?.close();
+				if (isRpcRequest) {
+					// Parse throw an error if not expected msg.
+					const notification = IcrcWalletNotification.parse(data);
+					console.log(notification);
+
+					const msg: IcrcWalletPermissionsRequestType = {
+						jsonrpc: JSON_RPC_VERSION_2,
+						method: ICRC25_REQUEST_PERMISSIONS,
+						params: {
+							scopes: [{ method: ICRC27_GET_ACCOUNTS }]
+						}
+					};
+
+					popup?.postMessage(msg, { targetOrigin: origin });
+					return;
+				}
+
+				const response = RpcResponse.parse(data);
+
+				console.log('RESULT ----> ', response.result);
+
+				// TODO: I'm not convinced by this pattern. Really handy but, not beautiful
+				popup?.close();
+
+				window.removeEventListener('message', onMessage);
 
 				const wallet = new IcrcWallet(origin);
 				resolve(wallet);
