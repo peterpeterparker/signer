3ac154dc9808858ee5b4dedaca150cd23b231d43
David Dal Busco <david.dalbusco@outlook.com>
2024-06-03 14:49:33 +0200

    feat: send icp

---
+++ scripts/send.tokens.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Utility Script: Send Tokens to a Local User
+# Particularly handy after installing the canisters for the first time or being forced by dfx to clean the local state.
+
+if [ -z "$1" ]; then
+  read -r -p "Enter the PRINCIPAL: " PRINCIPAL
+else
+  PRINCIPAL=$1
+fi
+
+DFX_NETWORK=local
+
+dfx canister call icp_ledger --network "$DFX_NETWORK" icrc1_transfer "(record {from=null; to=record { owner= principal \"$PRINCIPAL\";}; amount=1020000000; fee=null; memo=null; created_at_time=null;})"
